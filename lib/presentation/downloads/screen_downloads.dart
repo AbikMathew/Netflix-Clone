60dd001e73068d0dbb538a3de96c6df105e8433b
Abik Mathew George <abhikmgeorge100@gmail.com>
2022-05-05 08:57:17 +0530

    Downloads navbar + Google fonts

--- lib/presentation/downloads/screen_downloads.dart
+++ lib/presentation/downloads/screen_downloads.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_clone/presentation/widgets/appbar_widget.dart';
 
 class ScreenDownloads extends StatelessWidget {
   const ScreenDownloads({ Key? key }) : super(key: key);
@@ -6,6 +7,9 @@ class ScreenDownloads extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(40),
+        child: const AppbarWidget()),
       body: Text('Downloads'),
     );
   }
--- lib/presentation/widgets/appbar_widget.dart
+++ lib/presentation/widgets/appbar_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_clone/core/constants.dart';
 
 class AppbarWidget extends StatelessWidget {
   const AppbarWidget({ Key? key }) : super(key: key);
@@ -6,7 +7,12 @@ class AppbarWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child: Row(children: [
+        Expanded(child: Text('Downloads')),
+        IconButton(onPressed: (){}, icon: Icon(Icons.cast, color: Colors.white,)),
+        Container(width: 24,height: 24,color: Colors.blue,),
+        kWidth,
+      ],),
     );
   }
 }
